33f950ea638a232bca73d7a5237376f03c112ac0
Michał Tatarynowicz <tatarynowicz@gmail.com>
2015-10-20 18:58:38 +0200

    Fixed phpunit bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,14 +3,14 @@
 namespace Test;
 
 $possibleAutoloadFiles = [
-    __DIR__ . '/../../../autoload.php',
-    __DIR__ . '/vendor/autoload.php',
+    __DIR__.'/../vendor/autoload.php',
+    __DIR__.'/../../../autoload.php',
 ];
 
 foreach ($possibleAutoloadFiles as $possibleAutoloadFile) {
     if (file_exists($possibleAutoloadFile)) {
         $loader = require $possibleAutoloadFile;
-        $loader->addPsr4('Assertis\\SimpleDatabase\\', __DIR__);
+        $loader->addPsr4('Assertis\\Util\\', __DIR__);
     }
 }
 
