f46926faa1cab74cda7feb292f41773c4ccf7524
Michał Tatarynowicz <tatarynowicz@gmail.com>
2015-10-27 18:18:04 +0100

    Added searching to SimpleFactory.

--- src/SimpleFactory.php
+++ src/SimpleFactory.php
@@ -66,4 +66,50 @@ abstract class SimpleFactory
         return array_map([$this, 'fromArray'], $data);
     }
 
+    /**
+     * @param array $parameters
+     * @return string
+     */
+    private function parametersToQuery(array $parameters)
+    {
+        if (empty($parameters)) {
+            return '1=1';
+        }
+
+        $out = [];
+        foreach ($parameters as $key => $value) {
+            $out[] = "({$key} = :{$key})";
+        }
+        return join(' AND ', $out);
+    }
+
+    /**
+     * @param string $table
+     * @param array $parameters
+     * @param string $order
+     * @param int $page
+     * @param int $limit
+     * @return mixed[]
+     */
+    protected function getSearchResultsByParameters($table, array $parameters, $order, $page, $limit)
+    {
+        $offset = ($page - 1) * $limit;
+        $query = $this->parametersToQuery($parameters);
+        $sql = "SELECT * FROM `{$table}` WHERE {$query} ORDER BY {$order} LIMIT {$offset},{$limit};";
+        
+        return $this->getAllByParameters($sql, $parameters);
+    }
+
+    /**
+     * @param string $table
+     * @param array $parameters
+     * @return int
+     */
+    public function getSearchResultsCountByParameters($table, array $parameters)
+    {
+        $query = $this->parametersToQuery($parameters);
+        $sql = "SELECT COUNT(*) FROM `{$table}` WHERE {$query};";
+
+        return (int)$this->getDb()->getColumn($sql, $parameters);
+    }
 }
